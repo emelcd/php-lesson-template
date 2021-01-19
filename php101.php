93fa111be77b285dcc35f6f8dca2317699221a78
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-01-19 08:49:37 +0100

    Added Some Exercises to php101.php

--- php101.php
+++ php101.php
@@ -14,7 +14,7 @@
     <style>
         body {
             background-color: #CFF4FC;
-            font-size: x-large;
+            /* font-size: x-large; */
         }
     </style>
     <div class="container">
@@ -54,7 +54,7 @@
         echo "<hr>"; //Linea Divisoria
         // Como saber si ha aprobado un alumno -> Variable de Control
         $media = 6.73;
-        echo" <b>Ej 1. ¿Cómo saber si ha aprobado? </b>
+        echo " <b>Ej 1. ¿Cómo saber si ha aprobado? </b>
         <br>Si $media es mayor o igual que 5 ha aprobado<br>";
 
         if ($media >= 5) {
@@ -97,12 +97,89 @@
             $radio = $_GET["radio"];
             $pi = 3.14159265358979323846;
             $circunferencia = 2 * $radio * $pi;
+            // echo random_int(100, 999);
 
-            echo "LA CIRCUNFERENCIA DE UN CÍRCULO CON RADIO $radio cm ES IGUAL A $circunferencia cm<hr>";
+            echo "LA CIRCUNFERENCIA DE UN CÍRCULO CON RADIO $radio cm ES IGUAL A $circunferencia cm";
         }
 
 
         ?>
+        <!-- Bucles FOR -->
+        <?php 
+        for ($i=0; $i < 3; $i++) { 
+            echo $i;
+            // 1B-0 < 3
+            // 2B-1 < 3
+            // 3B-2 < 3
+            // 4B-3 < 3 -- NO SE EJECUTA EL 4 BUCLE
+            // RESULTADO FINAL = 2, para 3 <=
+        }
+        ?>
+        <!-- FOR(A;B;C){EJECUTACODIGO} -->
+        <!-- A => POR DONDE EMPIEZA EL BUCLE-->
+        <!-- B => HASTA QUE B SE CUMPLA, SE EJECUTA EL CODIGO -->
+        <!-- C => TRANSFORMACIÓN DE "A" EN CADA BUCLE -->
+        
+        <?php
+        echo "<hr>SUMAR HASTA DIEZ   ";
+
+            for ($i=1; $i <= 10; $i++) { 
+                echo " $i ";
+            }
+        echo "<hr>";
+        echo "<b>Ej3. Una tienda necsita ver cuando empezará a ser rentable. Cada mes gana 100€ más que el anterior, pero pierde 23€ más. En el supuesto que este mes hubiera ganado 3203€ y hubiera perdido 4321€, ¿cuantos meses necesita?</b> ";
+        $gana = 3203;
+        $pierde = 4321;
+        for ($i=1; $i < 100; $i++) { 
+            $gana= $gana + 100;
+            $pierde = $pierde + 23;
+            // echo "<br>gana $gana > pierde $pierde -- MES $i";
+            // echo "$gana > $pierde $i";
+            if ($gana> $pierde) {
+                echo "<br>NECESITA $i meses<br>";
+                break;
+            }
+        }
+        // Si te has dado cuenta, hay algo que no encaja, no sabemos las vueltas que tiene que realizar hasta completar el Objetivo. Para solucionarlo utilizaremos un Bucle WHILE
+        $gana = 3203;
+        $pierde = 4321;
+        $i = 0;
+        while ($gana<$pierde) {
+            $i++;
+            $gana= $gana + 100;
+            $pierde = $pierde + 23;
+            // echo "<br>gana $gana > pierde $pierde -- MES $i";
+
+        }
+        echo "Necesita $i Meses<hr>";
+
+        // ARRAYS
+        // UN ARRAY ES UN TIPO DE VARIABLE QUE CONTIENE UN CONJUTO DE VARIABLES
+        // $ARRAY = array(string, int, float, bool, ...)
+        $arrayAsignaturas = array("MATES", "CASTELLANO", "DIBUJO");
+        // PARA ACCEDER A CADA UNA DE LAS VARIABLES 
+        // $arrayAsignaturas[0] -- MATES
+        $primeraAsignatura = $arrayAsignaturas[0];
+        $segundaAsignatura = $arrayAsignaturas[1];
+        $terceraAsignatura = $arrayAsignaturas[2];
+
+        echo $primeraAsignatura;
+        echo $segundaAsignatura;
+        echo $terceraAsignatura;
+
+        // MÉTODO ESPECIFICO PARA RECORRER ARRAYS DE ARRAY 
+
+        foreach ($arrayAsignaturas as $asignatura) {
+            echo " <br>$asignatura ";
+        }
+
+
+
+
+
+        
+        ?>
+
 
     </div>
 </body>
