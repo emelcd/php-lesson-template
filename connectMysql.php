17b71da67d68c4c4478137c22cb985e80256ef5e
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-02-14 03:07:21 +0100

    Added MySQL table VIEWER

--- connectMysql.php
+++ connectMysql.php
@@ -10,9 +10,18 @@
 
 <body>
     <style>
+        * {
+            font-family: monospace;
+            font-size: 1.1rem;
+        }
+
         table {
+            position: relative;
             margin: auto;
-            
+            display: block;
+            height: 500px;
+            overflow-y: scroll;
+
             border-collapse: collapse;
             border-spacing: 0;
             border: 1px solid #ddd;
@@ -20,7 +29,9 @@
 
         }
 
-        td,tr,th {
+        td,
+        tr,
+        th {
             max-width: 250px;
             overflow: hidden;
             text-overflow: ellipsis;
@@ -38,14 +49,26 @@
 
         .fixing {
             overflow-x: auto;
+            overflow-y: visible;
         }
+
         .title {
             text-align: center;
         }
+        .fixedlist {
+            text-align: justify;
+            margin: auto;
+            width: 30%;
+
+
+        }
+
+
     </style>
     <h1 class="title">MySQL TABLE VIEWER</h1>
     <form method="get">
         <input type="text" name="tableName" id="tableName">
+        <input type="number" name="viewCount" id="viewCount" value="50">
         <input type="submit">DALE
     </form>
 
@@ -55,23 +78,29 @@
 
     $servername = "localhost";
     $user = "root";
-    $password = "foofoo";
+    $password = "fooo";
     $myDB = "sakila";
 
+
+    $conn = new mysqli($servername, $user, $password, $myDB);
+
+
+
+    if ($conn->connect_error) {
+        die("Connection FAIL" . $conn->connect_error);
+    }
+
+
     if (isset($_GET['tableName'])) {
-        $conn = new mysqli($servername, $user, $password, $myDB);
-
-        if ($conn->connect_error) {
-            die("Connection FAIL" . $conn->connect_error);
-        }
 
 
-        $sql = "SELECT * FROM " . $_GET['tableName'] . " LIMIT 20";
+
+        $sql = "SELECT * FROM " . $_GET['tableName'] . " LIMIT " . $_GET['viewCount'];
 
 
         $result =  $conn->query($sql);
         $info_campo = mysqli_fetch_fields($result);
-        echo ("<h2 style='text-align:center;margin:auto;'>".$_GET['tableName']."</h2><div class='fixing'><table><tr>");
+        echo ("<h2 style='text-align:center;margin:auto;'>" . $_GET['tableName'] . "</h2><div class='fixing'><table><tr>");
 
         foreach ($info_campo as $campo) {
             echo ("<th>" . $campo->name) . "</th>";
@@ -87,21 +116,20 @@
         }
 
         echo ("</table></div>");
-
-        echo("<h1 style='text-align:center'>AVALIBLE TABLES</h1>");
-
-        $sql_showtables="SHOW TABLES";
-        $result2 = $conn->query($sql_showtables);
-        echo("<div class='fixing'><table>");
-        foreach ($result2 as $table) {
-            echo("<tr>");
-            foreach($table as $table_entry) {
-                echo("<th>".$table_entry."</th>");
-            }
-            echo("</tr>");
-        }
-        echo("</table></div>");
     };
+    echo ("<div class='fixedlist'><h1 style='text-align:center'>AVALIBLE TABLES (in)-><span style='color:blue;'> " . $myDB . "</span></h1>");
+    $sql_showtables = "SHOW TABLES";
+    $result2 = $conn->query($sql_showtables);
+    foreach ($result2 as $table) {
+        
+        foreach ($table as $table_entry) {
+            echo("<li>");
+            echo ( $table_entry );
+            echo("</li>");
+        }
+
+    }
+    echo("</div>")
     ?>
 
 </body>
