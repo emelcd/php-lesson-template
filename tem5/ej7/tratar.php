ad9f1ab8d29de64365c85ce2140176f2b02d4cd4
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-02-15 22:37:31 +0100

    ADDED NEW FILES

--- tem5/ej7/tratar.php
+++ tem5/ej7/tratar.php
@@ -3,20 +3,15 @@
 
     if(isset($_POST['bajar'])) {
         $_SESSION['valor'] = $_SESSION['valor'] - 1;
-        setcookie('valor', $_SESSION['valor']);
-        echo($_SESSION['valor']);
     }
     if(isset($_POST['subir'])) {
         $_SESSION['valor'] = $_SESSION['valor'] + 1;
-        setcookie('valor', $_SESSION['valor']);
-        echo($_SESSION['valor']);
     }
     if(isset($_POST['ini'])) {
         $_SESSION['valor'] = 0;
-        setcookie('valor', $_SESSION['valor']);
-        echo($_SESSION['valor']);
     }
-
+    
+    setcookie('valor', $_SESSION['valor']);
     header("Location: index.php");
 
 ?>
