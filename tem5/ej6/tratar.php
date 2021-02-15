c296baa8c15879d591ef924b9b11f0e0c49a778a
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-02-15 22:35:16 +0100

    ADDED NEW FILES

--- tem5/ej6/tratar.php
+++ tem5/ej6/tratar.php
@@ -3,15 +3,12 @@
 
     if(isset($_POST['bajar'])) {
         $_COOKIE['valor'] = $_COOKIE['valor'] - 1;
-        echo($_COOKIE['valor']);
     }
     if(isset($_POST['subir'])) {
         $_COOKIE['valor'] = $_COOKIE['valor'] + 1;
-        echo($_COOKIE['valor']);
     }
     if(isset($_POST['ini'])) {
         $_COOKIE['valor'] = 0;
-        echo($_COOKIE['valor']);
     }
     setcookie('valor', $_COOKIE['valor']);
 
